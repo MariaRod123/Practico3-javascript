a9b90c1ded954a3c73b0654eed6fbe05d712f480
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-10-21 20:36:29 -0300

    Agregado ejercicio 5

---
+++ ejercicio5/script.js
@@ -0,0 +1,5 @@
+
+function obtenerNombreProducto(nombre_producto) {
+  let resultado_nombre = document.getElementById('resultado');
+  resultado_nombre.textContent = 'Nombre del producto: ' + nombre_producto;
+}
